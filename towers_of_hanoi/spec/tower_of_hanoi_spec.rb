4b20ec8c713c39632ea2048014097beccf84aedf
Priyesh Shah <priyesh.shah147@gmail.com>
2021-05-13 17:36:22 -0400

    Rspec for move

--- towers_of_hanoi/spec/tower_of_hanoi_spec.rb
+++ towers_of_hanoi/spec/tower_of_hanoi_spec.rb
@@ -3,7 +3,11 @@ require "tower_of_hanoi"
 describe "TowerOfHanoi" do 
     subject(:tower) {TowerOfHanoi.new(3)}
     describe "#move" do
-        
+        it "should ask user to input" do
+            # input = user_pick("2", :chomp)
+            # allow(tower).to receive(:gets).and_return(input)
+            expect { tower.move }.to output(/Enter a position/).to_stdout
+        end
     end
 
     describe "#won?" do
